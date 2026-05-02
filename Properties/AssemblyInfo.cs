d6b3f43a780391da152c27d5df9f42dedd548ee1
alex-voron <42248041+alex-voron@users.noreply.github.com>
2026-05-03 00:51:00 +0300

    up

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("Voronin Oleksandr")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Voronin Oleksandr")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0+15bad975975cd0ef59f0c99fa5370584ea1179cb")]
+[assembly: AssemblyProduct("ArticulMaster")]
+[assembly: AssemblyTitle("ArticulMaster")]
+[assembly: TargetPlatform("Windows7.0")]
+[assembly: SupportedOSPlatform("Windows7.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
